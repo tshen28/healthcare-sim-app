34a016109d3e830185299bb7781516052bc2757b
Taylor Shen <101141057+tshen28@users.noreply.github.com>
2026-01-20 16:03:54 -0800

    Adding Firebase auth configurations

---
+++ src/services/firebase.ts
@@ -0,0 +1,39 @@
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getAnalytics } from "firebase/analytics";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const EXPO_PUBLIC_FIREBASE_API_KEY = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+const EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN =
+  process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const EXPO_PUBLIC_FIREBASE_PROJECT_ID =
+  process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID;
+const EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET =
+  process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID =
+  process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const EXPO_PUBLIC_FIREBASE_APP_ID = process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
+const EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID =
+  process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
